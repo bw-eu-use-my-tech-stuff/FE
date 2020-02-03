4eb7d0b1d303f4ee56f731f8064f012c9825146c
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-03 13:54:49 +0000

    Login done.

--- yoursmineyours/src/components/Login.js
+++ yoursmineyours/src/components/Login.js
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
+import "../styles/Login.css";
 
-function Login(){
+function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
 
+  const changeHandler = event => {
+      setUser({
+          ...user, 
+          [event.target.name]: event.target.value
+      });
+  };
 
-    return(
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+  const submitHandler = event => {
+      event.preventDefault();
+      console.log(user);
+  }
+
+  return (
+    <div>
+      <form className="loginForm" onSubmit={event => submitHandler(event)}>
+        <label>Email:</label>
+        <input
+          type="text"
+          placeholder="email"
+          name="email"
+          onChange={event => changeHandler(event)}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          onChange={event => changeHandler(event)}
+        />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
---
+++ yoursmineyours/src/components/SearchForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function SearchForm(){
+
+    return(
+        <div>
+            <form>
+                <input 
+                placeholder="What do you need"
+                />
+                <button>Find</button>
+            </form>
+        </div>
+    )
+}
+
+export default SearchForm;
