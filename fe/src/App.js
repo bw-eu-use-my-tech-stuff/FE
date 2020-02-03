6de72473f315d3f06a54740c4a726a89df7fe8d4
blakelower <blakelower1999@gmail.com>
2020-02-03 19:47:54 +0700

    made home page

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Register from './components/Register';
+import Register from  './components/Register';
 import Home from './components/Home';
 import Login from './components/Login';
 
