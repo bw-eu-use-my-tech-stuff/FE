c5c21bc395791fc7533689b765ef8c30e83b439b
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-03 11:46:31 +0000

    Issue with react-router-dom. Need to resolved. Potentially due to npm/yarn conflict???

---
+++ fe/src/components/ItemsForRentList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ItemsForRentList(){
+   
+    return(
+        <div>
+            <h1>List of Items to rent.
+                Get request of available items. 
+                This will be on Homepage. 
+                It will be visible be populated by the items that owners input. 
+            </h1>
+        </div>
+    )
+}
+
+export default ItemsForRentList;
