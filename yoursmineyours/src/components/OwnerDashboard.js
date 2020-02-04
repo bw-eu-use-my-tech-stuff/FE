ce5a9613894aaf1f71e5b3c28a6550947acae035
eoinlynchcodes <eoin95lynch@gmail.com>
2020-02-04 14:32:27 +0000

    Coming together.

--- yoursmineyours/src/components/OwnerDashboard.js
+++ yoursmineyours/src/components/OwnerDashboard.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function OwnerDashboard(){
+
+
+    const [ item, setItem ] = useState({
+        title: '',
+        price: ''
+    });
+
+    const addItem = event => {
+        event.preventDefault();
+        console.log(item);
+    }
 
     return(
         <div>
             <h1>Here you are in the Owner Dashboard, looking at a screen, don't scream, life is obscene.</h1>
+
+            <form>
+                <h2>Rent your products. Earn money.</h2>
+                <label>Title:</label>
+                <input
+                placeholder="Title"
+                name="title"
+                />
+                <label>Price(Per day):</label>
+                <input
+                type="number"
+                placeholder="Price"
+                name="price"
+                />
+                <button onClick={addItem}>Add Item</button>
+
+            </form>
         </div>
+
+
     )
 }
 
